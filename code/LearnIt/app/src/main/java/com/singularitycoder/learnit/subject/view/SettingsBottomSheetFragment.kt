a35e8ef882e11ab296f58ba85a6c4a8e030fe142
Singularity Coder <codehithesh@gmail.com>
2024-10-05 09:19:36 +0530

    added slider settings

--- code/LearnIt/app/src/main/java/com/singularitycoder/learnit/subject/view/SettingsBottomSheetFragment.kt
+++ code/LearnIt/app/src/main/java/com/singularitycoder/learnit/subject/view/SettingsBottomSheetFragment.kt
@@ -7,12 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.SeekBar
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.singularitycoder.learnit.databinding.FragmentSettingsBottomSheetBinding
 import com.singularitycoder.learnit.helpers.collectLatestLifecycleFlow
 import com.singularitycoder.learnit.helpers.constants.remindMeInList
+import com.singularitycoder.learnit.helpers.onSafeClick
 import com.singularitycoder.learnit.helpers.setTransparentBackground
 import com.singularitycoder.learnit.subject.model.Subject
 import com.singularitycoder.learnit.subtopic.model.SubTopic
@@ -47,8 +49,20 @@ class SettingsBottomSheetFragment : BottomSheetDialogFragment() {
     private fun FragmentSettingsBottomSheetBinding.setupUI() {
         setTransparentBackground()
         tvHeader.text = "Settings"
-        layoutVolumeSlider.tvSliderTitle.isVisible = false
-        layoutShakeSensitivitySlider.tvSliderTitle.isVisible = false
+
+        layoutShakeSensitivitySlider.apply {
+            tvSliderTitle.isVisible = false
+            sliderCustom.min = 1
+            sliderCustom.max = 10
+            sliderCustom.progress = 5
+        }
+
+        layoutVolumeSlider.apply {
+            tvSliderTitle.isVisible = false
+            sliderCustom.min = 1
+            sliderCustom.max = 10
+            sliderCustom.progress = 5
+        }
 
         etOnShake.editText?.setText(remindMeInList.first())
         val onShakeAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, remindMeInList)
@@ -62,6 +76,50 @@ class SettingsBottomSheetFragment : BottomSheetDialogFragment() {
     @SuppressLint("NotifyDataSetChanged")
     private fun FragmentSettingsBottomSheetBinding.setupUserActionListeners() {
         switchDefaultAlarmTone.setOnCheckedChangeListener { buttonView, isChecked ->
+        }
+
+        layoutShakeSensitivitySlider.apply {
+            ibReduce.onSafeClick {
+                sliderCustom.progress -= 1
+                tvShakeSensitivity.text = "Shake Sensitivity: ${sliderCustom.progress}"
+            }
+            ibIncrease.onSafeClick {
+                sliderCustom.progress += 1
+                tvShakeSensitivity.text = "Shake Sensitivity: ${sliderCustom.progress}"
+            }
+            sliderCustom.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    tvShakeSensitivity.text = "Shake Sensitivity: ${seekBar.progress}"
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) = Unit
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+                    println("seekbar progress: ${seekBar.progress}")
+                    tvShakeSensitivity.text = "Shake Sensitivity: ${seekBar.progress}"
+                }
+            })
+        }
+
+        layoutVolumeSlider.apply {
+            ibReduce.onSafeClick {
+                sliderCustom.progress -= 1
+                tvVolume.text = "Volume: ${sliderCustom.progress}"
+            }
+            ibIncrease.onSafeClick {
+                sliderCustom.progress += 1
+                tvVolume.text = "Volume: ${sliderCustom.progress}"
+            }
+            sliderCustom.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    tvVolume.text = "Volume: ${seekBar.progress}"
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) = Unit
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+                    println("seekbar progress: ${seekBar.progress}")
+                    tvVolume.text = "Volume: ${seekBar.progress}"
+                }
+            })
         }
     }
 
